30647333636504cdde4cd3b27d035603e33ff16e
Razvan <razvan.b.tudorache@gmail.com>
2017-06-01 16:53:36 +0300

    - update webconfig

--- AdministrationApp_SPP/Application/app/scripts/login/login.controller.js
+++ AdministrationApp_SPP/Application/app/scripts/login/login.controller.js
@@ -10,19 +10,21 @@
   function loginController($scope, $state, $mdDialog) {
     $scope.loginUser = function (ev) {
 
-      if ($scope.username === 'test' && $scope.password === 'test') {
-        $state.go('generalInformation');
-      } else {
-        $mdDialog.show(
-          $mdDialog.alert()
-            .parent(angular.element(document.body))
-            .clickOutsideToClose(true)
-            .title('Login problem!')
-            .textContent('The username or password are incorrect! Please try again.')
-            .ok('OK')
-            .targetEvent(ev)
-        );
+      if ($scope.loginForm.$valid === true) {
+        if ($scope.username === 'test' && $scope.password === 'test') {
+          $state.go('generalInformation');
+        } else {
+          $mdDialog.show(
+            $mdDialog.alert()
+              .parent(angular.element(document.body))
+              .clickOutsideToClose(true)
+              .title('Login problem!')
+              .textContent('The username or password are incorrect! Please try again.')
+              .ok('OK')
+              .targetEvent(ev)
+          );
+        }
       }
-    }
+    };
   }
 })();
